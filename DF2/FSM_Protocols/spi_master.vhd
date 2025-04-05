76a8d374a0665e896732d65d53eb7b3e0c96a852
IhorYashan <yashan.i@ajax.systems>
2025-04-05 20:44:41 +0300

    [SPI] Added separate regs

--- DF2/FSM_Protocols/spi_master.vhd
+++ DF2/FSM_Protocols/spi_master.vhd
@@ -19,28 +19,30 @@ END SPI_MASTER;
 
 ARCHITECTURE BEHAVIORAL OF SPI_MASTER IS
     TYPE StateType IS (IDLE, TRANSMIT, COMPLETE);
-    SIGNAL State        : StateType := IDLE;
-    SIGNAL ShiftReg     : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
-    SIGNAL DataReg     : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '0');
-    SIGNAL BitCount     : INTEGER RANGE 0 TO N := 0;
-    SIGNAL SCK_Reg      : STD_LOGIC := '1';
-    SIGNAL SCK_Counter  : INTEGER RANGE 0 TO 1 := 0;
+    SIGNAL State         : StateType := IDLE;
+    SIGNAL ShiftReg_TX   : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+    SIGNAL ShiftReg_RX   : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL BitCount      : INTEGER RANGE 0 TO N := 0;
+    SIGNAL SCK_Reg       : STD_LOGIC := '1';
+    SIGNAL SCK_Counter   : INTEGER RANGE 0 TO 1 := 0;
 BEGIN
     PROCESS(CLK_i, Reset_i)
     BEGIN
         IF Reset_i = '1' THEN
             State        <= IDLE;
-            ShiftReg     <= (OTHERS => '0');
+            ShiftReg_TX  <= (OTHERS => '0');
+            ShiftReg_RX  <= (OTHERS => '0');
             BitCount     <= 0;
             SCK_Reg      <= '1';
             SCK_Counter  <= 0;
+            Done_o       <= '0';
         ELSIF RISING_EDGE(CLK_i) THEN
             CASE State IS
                 WHEN IDLE =>
-                    SCK_Reg <= '1';
-                    Done_o  <= '0';
+                    SCK_Reg     <= '1';
+                    Done_o      <= '0';
                     IF Start_i = '1' THEN
-                        ShiftReg    <= InputData_i;
+                        ShiftReg_TX <= InputData_i;
                         BitCount    <= 0;
                         State       <= TRANSMIT;
                     END IF;
@@ -50,7 +52,8 @@ BEGIN
                         SCK_Reg     <= '0';
                         SCK_Counter <= 1;
                     ELSE
-                        ShiftReg    <= ShiftReg(N-2 DOWNTO 0) & MSIO_i;
+                        ShiftReg_RX <= ShiftReg_RX(N-2 DOWNTO 0) & MSIO_i;
+                        ShiftReg_TX <= ShiftReg_TX(N-2 DOWNTO 0) & '0';
                         BitCount    <= BitCount + 1;
                         SCK_Reg     <= '1';
                         SCK_Counter <= 0;
@@ -61,12 +64,11 @@ BEGIN
 
                 WHEN COMPLETE =>
                     State   <= IDLE;
-                    DataReg <= ShiftReg(N-1 DOWNTO 0);
                     Done_o  <= '1';
             END CASE;
         END IF;
     END PROCESS;
 
-    Mosi_o <= ShiftReg(N-1);
+    Mosi_o <= ShiftReg_TX(N-1);
     SCK_o  <= SCK_Reg;
 END BEHAVIORAL;
--- DF2/FSM_Protocols/spi_slave.vhd
+++ DF2/FSM_Protocols/spi_slave.vhd
@@ -17,36 +17,37 @@ ENTITY SPI_SLAVE IS
 END SPI_SLAVE;
 
 ARCHITECTURE BEHAVIORAL OF SPI_SLAVE IS
-    SIGNAL ShiftReg     : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+    SIGNAL ShiftReg_RX  : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '0');
+    SIGNAL ShiftReg_TX  : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '1');
     SIGNAL BitCount     : INTEGER RANGE 0 TO N := 0;
-    SIGNAL DataReg     : STD_LOGIC_VECTOR(N-1 DOWNTO 0):= (OTHERS => '0');
     SIGNAL Received_r   : STD_LOGIC := '0';
     SIGNAL received_r1  : STD_LOGIC := '0';
     SIGNAL received_r2  : STD_LOGIC := '0';
 BEGIN
 
-    
     PROCESS(SCK_i, Reset_i)
     BEGIN
         IF Reset_i = '1' THEN
-            ShiftReg     <= (OTHERS => '0');
-            BitCount     <= 0;
-            Received_r   <= '0';
+            ShiftReg_RX <= (OTHERS => '0');
+            ShiftReg_TX <= (OTHERS => '1');
+            BitCount    <= 0;
             OutputData_o <= (OTHERS => '0');
+            Received_r  <= '0';
+            ShiftReg_TX <= "10101010"; 
         ELSIF FALLING_EDGE(SCK_i) THEN
-            ShiftReg <= ShiftReg(N-2 DOWNTO 0) & MOSI_i;
-            BitCount <= BitCount + 1;
+            ShiftReg_RX <= ShiftReg_RX(N-2 DOWNTO 0) & MOSI_i;
+            ShiftReg_TX <= ShiftReg_TX(N-2 DOWNTO 0) & '0';
+            BitCount    <= BitCount + 1;
             IF BitCount = N-1 THEN
-                OutputData_o <= ShiftReg(N-2 DOWNTO 0) & MOSI_i;
-                DataReg <= ShiftReg(N-2 DOWNTO 0) & MOSI_i;
+                OutputData_o <= ShiftReg_RX(N-2 DOWNTO 0) & MOSI_i;
                 Received_r   <= '1';
+                BitCount     <= 0;
             ELSE
-                Received_r   <= '0';
+                Received_r <= '0';
             END IF;
         END IF;
     END PROCESS;
 
-    
     PROCESS(clk_i)
     BEGIN
         IF rising_edge(clk_i) THEN
@@ -55,8 +56,7 @@ BEGIN
         END IF;
     END PROCESS;
 
-    
     Received_o <= received_r2;
-    MISO_o <= ShiftReg(N-1);
+    MISO_o <= ShiftReg_TX(N-1);
 
-END BEHAVIORAL;  
+END BEHAVIORAL;
