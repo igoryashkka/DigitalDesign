342c996d054539f3077e347f79d445cc20cfcf18
IhorYashan <yashan.i@ajax.systems>
2025-04-11 16:29:21 +0300

    [lookahead] fix sequantial Behavioral

--- DF2/FSM_Protocols/lookahd.vhd
+++ DF2/FSM_Protocols/lookahd.vhd
@@ -6,8 +6,6 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 ENTITY pgs_logic IS
     PORT ( A_i    : IN  STD_LOGIC;
            B_i    : IN  STD_LOGIC;
-           Cin_i  : IN  STD_LOGIC;
-           Sum_o  : OUT STD_LOGIC;
            P_o    : OUT STD_LOGIC;
            G_o    : OUT STD_LOGIC);
 END pgs_logic;
@@ -16,7 +14,6 @@ ARCHITECTURE Behavioral OF pgs_logic IS
 BEGIN
     P_o   <= (A_i XOR B_i); 
     G_o   <= (A_i AND B_i); 
-    Sum_o <= (A_i XOR B_i XOR Cin_i);
 END Behavioral;
 
 LIBRARY IEEE;
@@ -44,8 +41,6 @@ SIGNAL c    : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
 COMPONENT pgs_logic IS
    PORT (  A_i    : IN  STD_LOGIC;
            B_i    : IN  STD_LOGIC;
-           Cin_i  : IN  STD_LOGIC;
-           Sum_o  : OUT STD_LOGIC;
            P_o    : OUT STD_LOGIC;
            G_o    : OUT STD_LOGIC);
 END COMPONENT;
@@ -59,8 +54,12 @@ BEGIN
     END GENERATE gen_propagation_logic;
 
     gen_pgs_logic: FOR i IN 0 TO N-1 GENERATE
-        pgs_logic_inst: pgs_logic PORT MAP(A_i(i), B_i(i), c(i), S_o(i), p(i), g(i));
+        pgs_logic_inst: pgs_logic PORT MAP(A_i(i), B_i(i), p(i), g(i));
     END GENERATE gen_pgs_logic;
+
+    gen_sum: FOR i IN 0 TO N-1 GENERATE
+    S_o(i) <= p(i) XOR c(i);
+    END GENERATE gen_sum;
 
     Cout_o <= c(N-1);
 
