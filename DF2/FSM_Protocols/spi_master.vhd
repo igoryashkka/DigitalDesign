6aa46c6d03b0bad8ffcf44763148d2163fb46004
IhorYashan <yashan.i@ajax.systems>
2025-04-11 14:40:29 +0300

    [SPI] Added State <= TRANSMIT, avoid duplication SCK_counter

--- DF2/FSM_Protocols/spi_master.vhd
+++ DF2/FSM_Protocols/spi_master.vhd
@@ -20,12 +20,12 @@ END SPI_MASTER;
 ARCHITECTURE BEHAVIORAL OF SPI_MASTER IS
     TYPE StateType IS (IDLE, TRANSMIT, COMPLETE);
     SIGNAL State         : StateType := IDLE;
-    SIGNAL ShiftReg_TX   : STD_LOGIC_VECTOR(N-1 DOWNTO 0);
+    SIGNAL ShiftReg_TX   : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '0');
     SIGNAL ShiftReg_RX   : STD_LOGIC_VECTOR(N-1 DOWNTO 0) := (OTHERS => '0');
     SIGNAL BitCount      : INTEGER RANGE 0 TO N := 0;
-    SIGNAL SCK_Reg       : STD_LOGIC := '1';
-    SIGNAL SCK_Counter   : INTEGER RANGE 0 TO 1 := 0;
+    SIGNAL SCK_Reg       : STD_LOGIC := '0';
 BEGIN
+
     PROCESS(CLK_i, Reset_i)
     BEGIN
         IF Reset_i = '1' THEN
@@ -33,13 +33,13 @@ BEGIN
             ShiftReg_TX  <= (OTHERS => '0');
             ShiftReg_RX  <= (OTHERS => '0');
             BitCount     <= 0;
-            SCK_Reg      <= '1';
-            SCK_Counter  <= 0;
+            SCK_Reg      <= '0';
             Done_o       <= '0';
+
         ELSIF RISING_EDGE(CLK_i) THEN
             CASE State IS
                 WHEN IDLE =>
-                    SCK_Reg     <= '1';
+                    SCK_Reg     <= '0';
                     Done_o      <= '0';
                     IF Start_i = '1' THEN
                         ShiftReg_TX <= InputData_i;
@@ -48,27 +48,28 @@ BEGIN
                     END IF;
 
                 WHEN TRANSMIT =>
-                    IF SCK_Counter = 0 THEN
-                        SCK_Reg     <= '0';
-                        SCK_Counter <= 1;
-                    ELSE
+                    IF SCK_Reg = '0' THEN
                         ShiftReg_RX <= ShiftReg_RX(N-2 DOWNTO 0) & MSIO_i;
                         ShiftReg_TX <= ShiftReg_TX(N-2 DOWNTO 0) & '0';
-                        BitCount    <= BitCount + 1;
-                        SCK_Reg     <= '1';
-                        SCK_Counter <= 0;
+                        BitCount <= BitCount + 1;
+                        SCK_Reg <= '1';
                         IF BitCount = N - 1 THEN
                             State <= COMPLETE;
+                        ELSE 
+                            State <= TRANSMIT;
                         END IF;
+                    ELSE
+                        SCK_Reg <= '0';
                     END IF;
 
                 WHEN COMPLETE =>
-                    State   <= IDLE;
-                    Done_o  <= '1';
+                    Done_o <= '1';
+                    State  <= IDLE;
             END CASE;
         END IF;
     END PROCESS;
 
     Mosi_o <= ShiftReg_TX(N-1);
     SCK_o  <= SCK_Reg;
+
 END BEHAVIORAL;
