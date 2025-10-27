65e607eab47370925552b3a0dde1e1cb99b34339
IhorYashan <yashan.i@ajax.systems>
2025-10-27 18:14:31 +0200

    [DF3] Added dsp macro for bd

--- DF3/Decoder/sources/bd/design_1_wrapper.v
+++ DF3/Decoder/sources/bd/design_1_wrapper.v
@@ -1,9 +1,9 @@
 //Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
-//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
-//Tool Version: Vivado v.2025.1 (win64) Build 6140274 Thu May 22 00:12:29 MDT 2025
-//Date        : Fri Oct 24 01:13:47 2025
-//Host        : DESKTOP-IVJE1MH running 64-bit major release  (build 9200)
+//Tool Version: Vivado v.2024.2 (win64) Build 5239630 Fri Nov 08 22:35:27 MST 2024
+//Date        : Mon Oct 27 17:50:57 2025
+//Host        : DESKTOP-C9DG6FV running 64-bit major release  (build 9200)
 //Command     : generate_target design_1_wrapper.bd
 //Design      : design_1_wrapper
 //Purpose     : IP block netlist
