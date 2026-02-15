d0ccc755b38e380166c008a826a805b3aa9928bd
IhorYashan <yashan.i@ajax.systems>
2026-02-15 22:00:56 +0200

    [DF4][AXI] read init signal

--- DF4/sources/rtl/axi_gpio/axi_slave_gpio.vhd
+++ DF4/sources/rtl/axi_gpio/axi_slave_gpio.vhd
@@ -43,16 +43,15 @@ entity axi_lite_regs_if is
     s_axi_rvalid  : out std_logic;
     s_axi_rready  : in  std_logic;
     -----------------------------------------------------------
-    
     -- Register-file side
     axi_write_fire : out std_logic;
     wr_addr        : out std_logic_vector(ADDR_WIDTH-1 downto 0);
     wr_data        : out std_logic_vector(DATA_WIDTH-1 downto 0);
     wr_strb        : out std_logic_vector((DATA_WIDTH/8)-1 downto 0);
 
-    rd_en         : out std_logic;
-    rd_addr       : out std_logic_vector(ADDR_WIDTH-1 downto 0);
-    rd_data       : in  std_logic_vector(DATA_WIDTH-1 downto 0)
+    axi_read_fire  : out std_logic;
+    rd_addr        : out std_logic_vector(ADDR_WIDTH-1 downto 0);
+    rd_data        : in  std_logic_vector(DATA_WIDTH-1 downto 0)
   );
 end entity;
 architecture rtl of axi_lite_regs_if is
@@ -68,34 +67,49 @@ architecture rtl of axi_lite_regs_if is
   signal bresp_reg   : std_logic_vector(1 downto 0) := "00"; -- OKAY
 
 begin   
+  ---------------------------------------------------------------
+  -- AXI4-Lite output assignments and cobinatioanl logic
   s_axi_bresp <= bresp_reg;
+  s_axi_rresp <= rresp_reg;
+  
+  rd_addr <= rd_addr_reg;
+  s_axi_rdata <= rdata_reg;
 
   s_axi_awready <= '1' when (s_axi_bvalid='0' and aw_stored='0') else '0';
   s_axi_wready  <= '1' when (s_axi_bvalid='0' and w_stored='0')  else '0';
-
+  s_axi_arready <= '1' when (s_axi_rvalid='0' and read_pending='0') else '0';
+  -----------------------------------------------------------------------               
   -- Register-file side wiring
   wr_addr <= address_reg;
   wr_data <= wdata_reg;
   wr_strb <= wstrb_reg;
-
-
+  ----------------------------------------------------------------
   process (s_axi_aclk)
     variable aw_hand_shake : boolean;
     variable w_hand_shake  : boolean;
+    variable ar_hand_shake : boolean;
+    variable r_hand_shake  : boolean;
   begin
     if rising_edge(s_axi_aclk) then
       if s_axi_aresetn='0' then
-        address_reg     <= (others=>'0');
-        wdata_reg       <= (others=>'0');
-        wstrb_reg       <= (others=>'0');
+        -- write reset
+        address_reg    <= (others=>'0');
+        wdata_reg      <= (others=>'0');
+        wstrb_reg      <= (others=>'0');
+        aw_stored      <= '0';
+        w_stored       <= '0';
+        s_axi_bvalid   <= '0';
+        bresp_reg      <= "00";
+        axi_write_fire <= '0';
 
-        aw_stored       <= '0';
-        w_stored        <= '0';
-
-        s_axi_bvalid    <= '0';
-        bresp_reg       <= "00";
-
-        axi_write_fire  <= '0';
+        -- read reset
+        rd_addr_reg    <= (others=>'0');
+        rdata_reg      <= (others=>'0');
+        rresp_reg      <= "00";
+        s_axi_rvalid   <= '0';
+        read_pending   <= '0';
+        axi_read_fire  <= '0';
+        rd_en          <= '0';
       else
        -------------------------------------------------------------
        -- Write transaction handling
@@ -130,8 +144,33 @@ begin
         if (s_axi_bvalid='1' and s_axi_bready='1') then
           s_axi_bvalid <= '0';
         end if;
+        -------------------------------------------------------------   
+        -- Read  transaction handling
         -------------------------------------------------------------
+        axi_read_fire <= '0';
 
+        ar_hand_shake := (s_axi_arvalid='1' and s_axi_arready='1');
+        r_hand_shake  := (s_axi_rvalid='1'  and s_axi_rready='1');
+
+        -- Capture AR
+        if ar_hand_shake then
+          rd_addr_reg  <= s_axi_araddr;
+          axi_read_fire <= '1';
+          read_pending <= '1';
+        end if;
+
+        if (read_pending='1' and s_axi_rvalid='0') then
+          rdata_reg     <= rd_data; -- data from reg-file
+          axi_read_fire <= '1';     -- flag to reg-file
+          s_axi_rvalid  <= '1';     -- response pending
+          rresp_reg     <= "00";    -- OKAY, 0x00 for OK
+          read_pending  <= '0';     -- clear flag ar
+        end if;
+
+        if r_hand_shake then
+          s_axi_rvalid <= '0';
+        end if;
+        
       end if;
     end if;
   end process;
