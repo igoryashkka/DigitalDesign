973e0c6d15f01f58d1dd82bf03ef508a60a62b6c
IhorYashan <yashan.i@ajax.systems>
2026-02-16 15:42:28 +0200

    [DF4] update wraper hdl top on origin

--- DF4/sources/bd/microblaze_wrapper.v
+++ DF4/sources/bd/microblaze_wrapper.v
@@ -2,7 +2,7 @@
 //Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
-//Date        : Wed Feb 11 23:20:31 2026
+//Date        : Mon Feb 16 14:46:22 2026
 //Host        : DESKTOP-C9DG6FV running 64-bit major release  (build 9200)
 //Command     : generate_target microblaze_wrapper.bd
 //Design      : microblaze_wrapper
@@ -13,29 +13,29 @@
 module microblaze_wrapper
    (diff_clock_rtl_0_clk_n,
     diff_clock_rtl_0_clk_p,
-    led_1_0,
     pwm_r_o_0,
+    pwm_r_o_1,
     reset_rtl_0,
     rst_n_0);
   input diff_clock_rtl_0_clk_n;
   input diff_clock_rtl_0_clk_p;
-  output led_1_0;
   output pwm_r_o_0;
+  output pwm_r_o_1;
   input reset_rtl_0;
   input rst_n_0;
 
   wire diff_clock_rtl_0_clk_n;
   wire diff_clock_rtl_0_clk_p;
-  wire led_1_0;
   wire pwm_r_o_0;
+  wire pwm_r_o_1;
   wire reset_rtl_0;
   wire rst_n_0;
 
   microblaze microblaze_i
        (.diff_clock_rtl_0_clk_n(diff_clock_rtl_0_clk_n),
         .diff_clock_rtl_0_clk_p(diff_clock_rtl_0_clk_p),
-        .led_1_0(led_1_0),
         .pwm_r_o_0(pwm_r_o_0),
+        .pwm_r_o_1(pwm_r_o_1),
         .reset_rtl_0(reset_rtl_0),
         .rst_n_0(rst_n_0));
 endmodule
