d86cabfe25645e826ad0a83c65a6b444b39447ea
igoryashkka <83956200+igoryashkka@users.noreply.github.com>
2026-02-17 13:44:06 +0200

    [DF4][VITIS] refact code

--- DF4/ws_vitis/app_component_xsa/src/main.c
+++ DF4/ws_vitis/app_component_xsa/src/main.c
@@ -1,55 +1,67 @@
-
 #include <stdio.h>
 #include "xil_printf.h"
 #include "xgpio.h"
 #include "xstatus.h"
 #include "sleep.h"
+#include "xil_io.h"
+#include "xil_types.h"
 
 
-#define REG1_CHANNEL 1
+#define REG1_CHANNEL      1
+#define GPIO_MASK_8BIT    0xFFu
 
-#define DATA_MASK 0xFF
-XGpio gpio_h;
-XGpio gpio_h1;
+#define DELAY_US          500000u   // 500 ms
 
-#define TOP_BASE      XPAR_TOP_GPIO_0_BASEADDR   // 0x00010000
-#define TOP_REG_OFF   0x00                       
-#define TOP_MASK      0xFF
+#define TOP_BASE          XPAR_TOP_GPIO_0_BASEADDR
+#define TOP_REG_OFF       0x00u
+#define TOP_MASK_8BIT     0xFFu
 
-static inline void top_write_u8(u32 v)
+static XGpio g_gpio_std;   
+static XGpio g_gpio_top;   
+
+// ------------------------------------------------------------
+// Custom AXI GPIO 
+static inline void top_write_u8(u8 v)
 {
-    Xil_Out32(TOP_BASE + TOP_REG_OFF, v & TOP_MASK);
+    Xil_Out32(TOP_BASE + TOP_REG_OFF, (u32)(v & TOP_MASK_8BIT));
 }
 
 static inline u32 top_read_u32(void)
 {
     return Xil_In32(TOP_BASE + TOP_REG_OFF);
 }
-
-void write_to_gpio(XGpio *confPtr, char channel, char data, char dataMask)
+// ------------------------------------------------------------
+// Standard AXI GPIO
+static int gpio_init(XGpio *inst, u32 device_id_or_baseaddr)
 {
-    char data_masked = data & dataMask;
-    XGpio_DiscreteWrite(confPtr, channel, data_masked);
+    return XGpio_Initialize(inst, device_id_or_baseaddr);
 }
 
-void Gpio_init(XGpio *confPtr, int base_addr)
+static inline void gpio_write_u8(XGpio *inst, u32 channel, u8 value)
 {
-    XGpio_Initialize(confPtr, base_addr);
+    XGpio_DiscreteWrite(inst, channel, (u32)value);
 }
-
+// ------------------------------------------------------------
+// Common function to set duty pwm
+static void set_duty_pwm(u8 duty_std, u8 duty_top)
+{
+    gpio_write_u8(&g_gpio_std, REG1_CHANNEL, (u8)(duty_std & GPIO_MASK_8BIT));
+    top_write_u8((u8)(duty_top & TOP_MASK_8BIT));
+}
+// ------------------------------------------------------------
 int main(void)
 {
-    Gpio_init(&gpio_h, XPAR_XGPIO_0_BASEADDR);
-    Gpio_init(&gpio_h1, XPAR_TOP_GPIO_0_BASEADDR);
+    int status;
+
+    status = gpio_init(&g_gpio_std, XPAR_XGPIO_0_BASEADDR);
+    status = gpio_init(&g_gpio_top, XPAR_TOP_GPIO_0_BASEADDR);
+
 
     while (1) {
-        write_to_gpio(&gpio_h, REG1_CHANNEL, 20U, DATA_MASK);
-        top_write_u8(200);
-        usleep(500000);   // 500 мс
+        set_duty_pwm(20u, 200u);
+        usleep(DELAY_US);
 
-        write_to_gpio(&gpio_h, REG1_CHANNEL, 200U, DATA_MASK);
-         top_write_u8(20);
-        usleep(500000);   // 500 мс
-        
+        set_duty_pwm(200u, 20u);
+        usleep(DELAY_US);
     }
 }
